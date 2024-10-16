7265df19c4075ae2f16ce2347c503b5b825a7ec3
Carlota Rodriguez Garcia <carlotaroga@hotmail.com>
2024-10-16 20:13:20 +0200

    next commit

--- Starting Python/index.py
+++ Starting Python/index.py
@@ -1,12 +1,37 @@
-fruit = 'apple'
-print(fruit)
+# fruit = 'apple'
+# print(fruit)
 
-fruit = 'pear'
-print(fruit)
+# fruit = 'pear'
+# print(fruit)
 
-5 + 2
+# 5 + 2
 
-sum = 5 + 2
-print(sum)
+# sum = 5 + 2
+# print(sum)
 
 
+
+# print('Hello world!')
+# print('What is your name?') # ask for their name
+# myName = input()
+# print('It is good to meet you, ' + myName)
+# print('The length of your name is:')
+# print(len(myName))
+# print('What is your age?') # ask for their age
+# myAge = input()
+# print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+
+
+print("What is your name?")
+name = input()
+print("How old are you?")
+age = input()
+
+if name == 'Alice' and int(age) < 13:
+    print("How's Wonderland?")
+elif name == 'Alice':
+    print("You are too old to enter through the looking glass, Alice.")
+elif int(age) < 13:
+    print('This is only for Alice, kiddo.')
+else:
+    print('You are not Alice, and you are 13 or older.')
